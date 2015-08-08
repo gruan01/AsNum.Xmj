da17f17f8e30c9d29c7dcfa5f0d66d941c5b0a0a
xlingfairy <gruan@asnum.com>
2015-08-08 11:03:49 +0800

    Add menu

---
+++ AsNum.Xmj.OrderManager/Menus/ShamShippingQuery.cs
@@ -0,0 +1,33 @@
+﻿using AsNum.Xmj.BizEntity.Conditions;
+using AsNum.Xmj.Common;
+using AsNum.Xmj.Common.Interfaces;
+using AsNum.Xmj.Entity;
+using AsNum.Xmj.OrderManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsNum.Xmj.OrderManager.Menus {
+    [Export(typeof(IMenuItem)), ExportMetadata("TopMenuTag", TopMenuTags.DataMQ)]
+    public class ShamShippingMenu : MenuItemBase {
+        public override string Header {
+            get {
+                return "已填单未发货";
+            }
+        }
+
+        public override void Execute(object obj) {
+
+            var cond = new OrderSearchCondition() {
+                Status = OrderStatus.WAIT_BUYER_ACCEPT_GOODS,
+                IsShamShipping = true
+            };
+            var vm = new OrderQueryViewModel();
+            vm.Search(cond);
+            vm.Show("已填单未发货的订单");
+        }
+    }
+}
